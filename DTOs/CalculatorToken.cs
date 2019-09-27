d81e5de985f13283613cdd3e316db2be5c73b774
MaxBeauchemin <maxbeauchemin@gmail.com>
2019-09-26 19:24:25 -0500

    Add default functionality for Calculator.Sum(..)

---
+++ DTOs/CalculatorToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.DTOs
+{
+    public class CalculatorToken
+    {
+        public string Token { get; set; }
+        public decimal Value { get; set; }
+        public bool Errored { get; set; }
+    }
+}
--- Services/Calculator.cs
+++ Services/Calculator.cs
@@ -1,9 +1,14 @@
 ﻿using Calculator.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Services
 {
     public class Calculator
     {
+        private int _upperBound = 1000;
+
         /// <summary>
         /// Sums up the numbers provided in the input, ignores numbers greater than 1000 and invalid inputs
         /// Default delimiters include , and linebreak, but custom delimiters can be used
@@ -12,7 +17,70 @@ namespace Services
         /// <returns></returns>
         public CalculatorResponse Sum(string input)
         {
-            return null;
+            var response = new CalculatorResponse { Success = true };
+
+            var tokens = Tokenize(input);
+
+            if (tokens.Any(t => t.Errored))
+            {
+                response.Success = false;
+
+                //TODO: Add valid error message for invalid tokens
+                response.Message = "ERROR";
+            }
+            else
+            {
+                response.Value = tokens.Sum(t => t.Value);
+            }
+
+            return response;
+        }
+
+        /// <summary>
+        /// Splits up the input string and returns all of the digits between the delimiters
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private List<CalculatorToken> Tokenize(string input)
+        {
+            var delimiters = new List<string> { ",", "\n" };
+
+            //TODO: Add custom delimiters from input, adjust start index of input string to parse
+
+            var rawTokens = input.Split(delimiters.ToArray(), StringSplitOptions.None);
+
+            var tokens = new List<CalculatorToken>();
+
+            foreach (var raw in rawTokens)
+            {
+                var token = new CalculatorToken
+                {
+                    Token = raw
+                };
+
+                decimal value;
+
+                if (Decimal.TryParse(raw, out value))
+                {
+                    token.Value = value;
+
+                    //Check lower bound (error)
+                    if (value < 0)
+                    {
+                        token.Errored = true;
+                    }
+
+                    //Check upper bound (act as 0)
+                    if (value > _upperBound)
+                    {
+                        token.Value = 0;
+                    }
+                }
+
+                tokens.Add(token);
+            }
+
+            return tokens;
         }
     }
 }
