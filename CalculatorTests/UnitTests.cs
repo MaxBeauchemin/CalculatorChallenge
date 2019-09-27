c9872305070117d188f89a42cba021129d9585b9
MaxBeauchemin <maxbeauchemin@gmail.com>
2019-09-26 19:06:22 -0500

    Added more Unit Tests for future functionality

--- CalculatorTests/UnitTests.cs
+++ CalculatorTests/UnitTests.cs
@@ -7,6 +7,8 @@ namespace CalculatorTests
     {
         private Services.Calculator _calculator = new Services.Calculator();
 
+        #region Sum()
+
         [TestMethod]
         public void SingleNumberTest()
         {
@@ -19,5 +21,98 @@ namespace CalculatorTests
             Assert.IsTrue(output.Success, output.Message);
             Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
         }
+
+        [TestMethod]
+        public void TwoNumbersCommaTest()
+        {
+            var input = "80,45";
+            var expectedOutput = 125;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void TwoNumbersLineBreakTest()
+        {
+            var input = "80\n45";
+            var expectedOutput = 125;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void InvalidNumberTest()
+        {
+            var input = "20,INVALID";
+            var expectedOutput = 20;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void MultiDelimiterTest()
+        {
+            var input = "20,62\n18";
+            var expectedOutput = 100;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void CustomSingleCharDelimiterTest()
+        {
+            var input = "//;\n2;5";
+            var expectedOutput = 7;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void CustomMultiCharDelimiterTest()
+        {
+            var input = "//[***]\n11***22***33";
+            var expectedOutput = 66;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        [TestMethod]
+        public void MultipleCustomDelimitersTest()
+        {
+            var input = "////[*][!!][r9r]\n11r9r22*33!!44";
+            var expectedOutput = 110;
+
+            var output = _calculator.Sum(input);
+
+            Assert.IsNotNull(output, "Output object should not be null");
+            Assert.IsTrue(output.Success, output.Message);
+            Assert.AreEqual(expectedOutput, output.Value, "Incorrect Output");
+        }
+
+        #endregion
     }
 }
